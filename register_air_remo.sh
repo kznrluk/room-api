4c09ecd6fb56ae23214a8bad5b0a31d00e386c04
kznrluk <kznr.luk@gmail.com>
2020-04-07 00:16:59 +0900

    間隔を長く

--- register_air_remo.sh
+++ register_air_remo.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-airtarget=(airauto aircool airheat airdry)
+airtarget=(airauto aircool airdry airheat)
 
 for target in ${airtarget[@]}
 do
-  sleep 3
+  sleep 8
   for ((i=17; i<31; i++)) do
     echo irrp.py -r -g18 -f codes.json $target$i --no-confirm --post 130
     python irrp.py -r -g18 -f codes.json $target$i --no-confirm --post 130
